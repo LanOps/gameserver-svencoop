ca99bd85ad8d6e3655356870464d5ac2dd454b3e
Th0rn0 <dev@th0rn0.co.uk>
2019-03-15 01:41:52 +0000

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,43 @@
 FROM lanopsdev/gameserver-steamcmd:latest
 MAINTAINER Thornton Phillis (Th0rn0@lanops.co.uk)
 
-# Env Defaults
+# Env - Defaults
 ENV SRCDS_HOSTNAME default
 ENV SRCDS_PORT 27015 
 ENV SRCDS_MAXPLAYERS 8 
-ENV SRCDS_RCONPW default 
+ENV SRCDS_RCONPW rconpass 
 ENV SRCDS_REGION -1
 ENV SRCDS_PURE 1
 ENV SRCDS_MAP hl_c00
 ENV SRCDS_LAN 0
-ENV APP_ID 276060
+
+# Env - Server
+
+ENV SRCDS_SRV_DIR /home/steam/sven
+ENV SRCDS_APP_ID 276060
 
 # Add Start Script
-RUN mkdir -p /home/steam/sven 
+
+RUN mkdir -p ${SRCDS_SRV_DIR} 
 RUN { \
         echo '@ShutdownOnFailedCommand 1'; \
         echo '@NoPromptForPassword 1'; \
         echo 'login anonymous'; \
-        echo 'force_install_dir /home/steam/sven/'; \
-        echo 'app_update $APP_ID'; \
+        echo 'force_install_dir $SRCDS_SRV_DIR'; \
+        echo 'app_update $SRCDS_APP_ID'; \
         echo 'quit'; \
 } > /home/steam/sven_update.txt
 
 # Add Start Script
+
 ADD resources/root/startServer.sh /home/steam/startServer.sh
 
 # Expose Ports
+
 EXPOSE ${SRCDS_PORT}
 EXPOSE ${SRCDS_PORT}/udp
 EXPOSE 27020 27005 51840
 
 # Start Server
+
 CMD ["/home/steam/startServer.sh"]
--- resources/root/startServer.sh
+++ resources/root/startServer.sh
@@ -1,24 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /home/steam/sven
-/home/steam/steamcmd/steamcmd.sh +login anonymous   \
-        +force_install_dir /home/steam/sven          \
-        +app_update ${APP_ID} validate              \
+
+# Run Server
+
+/home/steam/steamcmd/steamcmd.sh +login anonymous       \
+        +force_install_dir ${SRCDS_SRV_DIR}             \
+        +app_update ${SRCDS_APP_ID} validate            \
         +quit
-./svends_run \
-    -console \
-    -autoupdate \
-    -steam_dir /home/steam/steamcmd/ \
-    -steamcmd_script /home/steam/sven/sven_update.txt \
-    -port ${SRCDS_PORT} \
-    -num_edicts 3072 \
-    -nohltv \
-    +sv_pure ${SRCDS_PURE} \
-    +maxplayers ${SRCDS_MAXPLAYERS} \
-    +hostname "${SRCDS_HOSTNAME}" \
-    +sv_password ${SRCDS_PW} \
-    +rcon_password ${SRCDS_RCONPW} \
-    sv_region ${SRCDS_REGION} \
-    +sv_lan "${SRCDS_LAN}" \
-    +sv_setsteamaccount ${SRCDS_TOKEN} \
+./svends_run                                            \
+    -console                                            \
+    -autoupdate                                         \
+    -steam_dir /home/steam/steamcmd/                    \
+    -steamcmd_script /home/steam/sven_update.txt   \
+    -port ${SRCDS_PORT}                                 \
+    -num_edicts 3072                                    \
+    -nohltv                                             \
+    +sv_pure ${SRCDS_PURE}                              \
+    +maxplayers ${SRCDS_MAXPLAYERS}                     \
+    +hostname "${SRCDS_HOSTNAME}"                       \
+    +sv_password ${SRCDS_PW}                            \
+    +rcon_password ${SRCDS_RCONPW}                      \
+    sv_region ${SRCDS_REGION}                           \
+    +sv_lan ${SRCDS_LAN}                                \
+    +sv_setsteamaccount ${SRCDS_TOKEN}                  \
     +map ${SRCDS_MAP}
